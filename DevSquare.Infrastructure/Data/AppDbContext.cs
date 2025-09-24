0b5458c2498d8729d9bac401ba2ab96b154b2e99
Mazen Elnems <mazenelnems22@gmail.com>
2025-09-24 08:54:41 +0300

    install EFCore Package and add DbContext
    - register AppDbContext in service collection in program.cs
    - add connection string into appsettings.development.json file

---
+++ DevSquare.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using DevSquare.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSquare.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):
+            base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
