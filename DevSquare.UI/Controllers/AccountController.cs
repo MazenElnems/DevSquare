65013d93c51aec5195b80082f4be7690f37c8ffe
Mazen Elnems <mazenelnems22@gmail.com>
2025-09-25 11:26:12 +0300

    add AccountController
    - add Login Action method
    - add Register Actoin method
    - add Logout Action method
    - add ResetPassword Action method
    - add Login,Register,RestPassword ViewModels

---
+++ DevSquare.UI/Controllers/AccountController.cs
@@ -0,0 +1,140 @@
+﻿using DevSquare.Core.Domain;
+using DevSquare.UI.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSquare.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string? ReturnUrl)
+        {
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel, string? ReturnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, isPersistent: loginViewModel.RemeberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(ReturnUrl) && Url.IsLocalUrl(ReturnUrl))
+                    {
+                        return LocalRedirect(ReturnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                ModelState.AddModelError(string.Empty, error.ErrorMessage);
+            }
+            return View(loginViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User()
+                {
+                    UserName = registerViewModel.UserName,
+                };
+
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                ModelState.AddModelError(string.Empty, error.ErrorMessage);
+            }
+            return View(registerViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("reset-password")]  
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("confirm-reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel resetPasswordViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string userName = User.Identity?.Name ?? string.Empty;
+                
+                User? user = await _userManager.FindByNameAsync(userName);
+
+                if(user == null)
+                {
+                    // TODO: Log Warning
+                    return RedirectToAction(nameof(Login));
+                }
+
+                var result = await _userManager.ChangePasswordAsync(user, resetPasswordViewModel.CurrentPassword, resetPasswordViewModel.NewPassword);
+
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignOutAsync();
+                    return RedirectToAction(nameof(Login));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+            {
+                ModelState.AddModelError(string.Empty, error.ErrorMessage);
+            }
+            return View(resetPasswordViewModel);
+        }
+    }
+}
---
+++ DevSquare.UI/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DevSquare.Core.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevSquare.UI.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "User name is required.")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Confirm Password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
